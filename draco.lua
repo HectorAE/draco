a1cfc8a6119d62e6dc56edc71c02180674d9e780
Hector A Escobedo <ninjahector.escobedo@gmail.com>
2013-06-04 18:20:03 -0400

    Added new attributes for gendragon. Gendragon now works as intended, returning a keyed table. In the future this may be serialized or used to init objects.
    Next to work on a data file readin for breath types, etc.

--- draco.lua
+++ draco.lua
@@ -1,16 +1,42 @@
 -- Globally accessible package that provides Draco's core functions
 -- Coded by Hector Escobedo
 
-local function gendragon ()
-   local breathtype = "fire"
-   return breathtype
+local breathtable = {
+   [1] = "fire",
+   [2] = "ice",
+   [3] = "acid",
+}
+
+local function randbreathtype ()
+   math.randomseed(os.time())	-- Kludgy but workable
+   local int = math.random(# breathtable) -- 1 to length of table
+   return breathtable[int]
 end
 
-local P = {			-- Our local package table
-   gendragon = gendragon
+local function randsex ()
+   math.randomseed(os.time())
+   local int = math.random(2)
+   if int == 1 then
+      return "female"
+   else
+      return "male"
+   end
+end
+
+local function gendragon ()	-- Main function for creating new dragons
+   local attr = {
+      ["breathtype"] = randbreathtype(),
+      ["sex"] = randsex(),
+   }
+   return attr
+end
+
+local P = {			-- Our package table to export
+   gendragon = gendragon,
 }
 
 -- Dynamic package name allocation for requires
+-- Allocates the called filename to package
 
 if _REQUIREDNAME == nil then
    draco = P			-- Default package name

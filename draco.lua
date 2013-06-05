a942dcfd4476a34281909d5da1691822515d7ab5
Hector A Escobedo <ninjahector.escobedo@gmail.com>
2013-06-04 20:47:27 -0400

    The gendragon functions now takes optional args from a table. It can also function with a nil argument. Sextable added for later.

--- draco.lua
+++ draco.lua
@@ -1,6 +1,10 @@
 -- Globally accessible package that provides Draco's core functions
 -- Coded by Hector Escobedo
 
+-- Don't try to generate dragons too fast until a better source of
+-- entropy is found. All random parameters are identical during the
+-- same second!
+
 local breathtable = {
    [1] = "fire",
    [2] = "ice",
@@ -13,26 +17,41 @@ local function randbreathtype ()
    return breathtable[int]
 end
 
+local sextable = {		-- Rather silly but useful
+   [1] = "female",
+   [2] = "male",
+}
+
 local function randsex ()
    math.randomseed(os.time())
-   local int = math.random(2)
-   if int == 1 then
-      return "female"
-   else
-      return "male"
-   end
+   local int = math.random(# sextable)
+   return sextable[int]
 end
 
-local function gendragon ()	-- Main function for creating new dragons
+-- Function that checks a dragon's attributes are all good with
+-- the standard trait lists.
+local function valdragon (attr)
+   return true 			-- Heck, it's all the same to me.
+end
+
+-- Main function for creating new dragons
+-- Takes a single indexed table (opt) with optional parameters for
+-- generation. All missing params (== nil) are randomized. This is
+-- one instance where or shortcut eval is very useful. Returns the
+-- final attribute table.
+local function gendragon (opt)
+   if opt == nil then
+      opt = {}			-- Still in local function, safe
+   end
    local attr = {
-      ["breathtype"] = randbreathtype(),
-      ["sex"] = randsex(),
+      ["breathtype"] = opt.breathtype or randbreathtype(),
+      ["sex"] = opt.sex or randsex(),
    }
    return attr
 end
 
 local P = {			-- Our package table to export
-   gendragon = gendragon,
+   gendragon = gendragon,	-- Pub name = local name
 }
 
 -- Dynamic package name allocation for requires

2093c127be31e34dc222a485e3e298cc434d2353
Hector A Escobedo <ninjahector.escobedo@gmail.com>
2013-06-17 19:29:55 -0400

    Framework for binding Irrlicht classes to Lua.
    More work needed on actual implementation.
    irr-exe.cpp will compile, but doesn't do anything as of yet.

--- draco-exe.lua
+++ draco-exe.lua
@@ -4,8 +4,8 @@
 -- Coded by Hector Escobedo
 
 require "draco"
-local lgi = require "lgi"
-local Gtk = lgi.Gtk
+-- local lgi = require "lgi"
+-- local Gtk = lgi.Gtk
 
 draco.getdata()
 
@@ -17,15 +17,29 @@ local idprint = dragon.name .. " is a " .. dragon.sex .. " " .. dragon.breathtyp
 
 -- print(idprint)
 
-local topwindow = Gtk.Window()
-topwindow.title = "Draco"
-function topwindow.on_destroy ()
-   Gtk.main_quit()
-end
+-- local topwindow = Gtk.Window()
+-- topwindow.title = "Draco"
+-- function topwindow.on_destroy ()
+--    Gtk.main_quit()
+-- end
 
-local idlabel = Gtk.Label({label = idprint})
+-- local idlabel = Gtk.Label({label = idprint})
 
-topwindow:add(idlabel)
-topwindow:show_all()
+-- topwindow:add(idlabel)
+-- topwindow:show_all()
 
-Gtk.main()
+-- Gtk.main()
+
+device = createDevice(EDT_OPENGL, dimension2d(400, 100), 16, false, false, false, 0)
+
+device.setWindowCaption("Draco")
+-- guienv.addStaticText(L"This is Draco, a label.",
+-- 		     rect<s32>(10,10,240,24), true)
+
+-- while device.run() do
+--    driver.beginScene(true, true, SColor(244,100,101,130))
+--    smgr.drawAll()
+--    guienv.drawAll()
+--    driver.endScene()
+-- end
+device.run()
--- irr-exe.cpp
+++ irr-exe.cpp
@@ -1,5 +1,12 @@
 #include <irrlicht.h>
-// #include <luabind/luabind.hpp>
+
+extern "C" {
+#include <lua5.1/lua.h>
+#include <lua5.1/lauxlib.h>
+#include <lua5.1/lualib.h>
+}
+
+#include <luabind/luabind.hpp>
 
 using namespace irr;
 
@@ -11,36 +18,51 @@ using namespace gui;
 
 // using namespace luabind;
 
-int main()
-{
-  // Start the Irrlicht engine
-  IrrlichtDevice *device =
-    createDevice( video::EDT_OPENGL, dimension2d<u32>(200, 350), 16,
-		  false, false, false, 0);
-  if (!device)
-    return 1;
+int main() {
+  // // Start the Irrlicht engine
+  // IrrlichtDevice *device =
+  //   createDevice( video::EDT_OPENGL, dimension2d<u32>(200, 350), 16,
+  // 		  false, false, false, 0);
+  // if (!device)
+  //   return 1;
 
-  device->setWindowCaption(L"Draco");
-  IVideoDriver* driver = device->getVideoDriver();
-  ISceneManager* smgr = device->getSceneManager();
-  IGUIEnvironment* guienv = device->getGUIEnvironment();
+  // Create our global Lua state
+  lua_State *L = luaL_newstate();
 
-  guienv->addStaticText(L"This is Draco, a label.",
-			rect<s32>(10,10,240,24), true);
+  // Connect it to luabind
+  luabind::open(L);
 
-  while(device->run())
-    {
-      driver->beginScene(true, true, SColor(244,100,101,130));
-      smgr->drawAll();
-      guienv->drawAll();
-      driver->endScene();
-    }
+  // // device->setWindowCaption(L"Draco");
+  // IVideoDriver* driver = device->getVideoDriver();
+  // ISceneManager* smgr = device->getSceneManager();
+  // IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-  device->drop();
+  // guienv->addStaticText(L"This is Draco, a label.",
+  // 			rect<s32>(10,10,240,24), true);
+
+  // while(device->run())
+  //   {
+  //     driver->beginScene(true, true, SColor(244,100,101,130));
+  //     smgr->drawAll();
+  //     guienv->drawAll();
+  //     driver->endScene();
+  //   }
+
+  // Bind the engine to the Lua state's global scope
+  luabind::module(L) [
+		      // luabind::class_<IrrlichtDevice>("IrrlichtDevice")
+		      luabind::def("createDevice", createDevice)
+		      // luabind::def("E_DRIVER_TYPE", E_DRIVER_TYPE)
+		      // luabind::def("dimension2d", dimension2d)
+		      ];
+
+  // Run the main Lua file
+  luaL_dostring(
+		L, "dofile('./draco-exe.lua')"
+		);
+
+  // device->drop();
+
+  lua_close(L);
   return 0;
 }
-
-// module(L)
-// [
-//  class_<dracoMainWindow>("topwindow")
-// ]
